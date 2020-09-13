a77a331d672227e72c73efe571ef05a8316a437d
fdz23 <ferdavid_9@hotmail.com>
2020-09-13 11:33:30 -0300

    Criação - Cria interface de medicos

---
+++ front-end/src/assets/medicos.ts
@@ -0,0 +1,14 @@
+export interface Medicos {
+  nome: string;
+  cpfcnpj: string;
+  rg: string;
+  registro: string;
+  endereco: string;
+  complemento: string;
+  numero: number;
+  cep: string;
+  uf: string;
+  cidade: string;
+  celular: string;
+  email: string;
+}
