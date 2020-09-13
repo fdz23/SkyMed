3aa8071567ec92b9a9aa01b597e288f00a737bc0
fdz23 <ferdavid_9@hotmail.com>
2020-09-12 23:51:35 -0300

    Criação - Cria dependencias e as declara

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateMedicoComponent } from './cruds/medicos/create-medico/create-medico.component';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RippleModule } from 'primeng/ripple';
+import { TabViewModule } from 'primeng/tabview';
+import { HeaderComponent } from './navegacao/header/header.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { FieldsetModule } from 'primeng/fieldset';
+import { AutoCompleteModule } from 'primeng/autocomplete';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateMedicoComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    InputTextModule,
+    CheckboxModule,
+    ButtonModule,
+    RadioButtonModule,
+    RippleModule,
+    FormsModule,
+    AppRoutingModule,
+    TabViewModule,
+    HttpClientModule,
+    TabMenuModule,
+    FieldsetModule,
+    AutoCompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
