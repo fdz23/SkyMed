9a6c51bc9944e53d9868cbb2837f4f4004402676
fdz23 <ferdavid_9@hotmail.com>
2020-09-25 17:18:00 -0300

    Correção - Deixa propriedades conforme o da api

--- front-end/src/assets/medicos.ts
+++ front-end/src/assets/medicos.ts
@@ -1,17 +1,14 @@
+import { Enderecos } from './enderecos';
+
 export interface Medicos {
   id: number;
   nome: string;
-  cpfcnpj: string;
+  cpf_cnpj: string;
   rg: string;
   tipoDeRegistro: string;
   registro: string;
   especialidade: string;
-  endereco: string;
-  complemento: string;
-  numero: number;
-  cep: string;
-  uf: string;
-  cidade: string;
-  celular: string;
+  endereco: Enderecos;
+  telefone: string;
   email: string;
 }
