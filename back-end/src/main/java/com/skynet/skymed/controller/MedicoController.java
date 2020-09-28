3950bed85e82ed4319f11a1121c03e94d63ff309
fdz23 <ferdavid_9@hotmail.com>
2020-09-28 17:35:50 -0300

    Criação - Criadas validações nas entidades

--- back-end/src/main/java/com/skynet/skymed/controller/MedicoController.java
+++ back-end/src/main/java/com/skynet/skymed/controller/MedicoController.java
@@ -1,51 +1,95 @@
 package com.skynet.skymed.controller;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.skynet.skymed.interfaces.IController;
 import com.skynet.skymed.model.Medico;
 import com.skynet.skymed.repository.MedicoRepository;
 
 @RestController
 
 @RequestMapping("/medico")
-public class MedicoController implements IController<Medico> {
+public class MedicoController {
 
 	@Autowired
 	private MedicoRepository medicoDB;
 
 	@GetMapping
-	@Override
-	public ArrayList<Medico> getObject() {
-		return (ArrayList<Medico>) medicoDB.findAll();
+	public ResponseEntity<ArrayList<Medico>> getObject() {
+		var medicos = medicoDB.findAll();
+		
+		if (medicos.size() == 0) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		
+		return ResponseEntity.ok((ArrayList<Medico>) medicos);
 	}
 
 	@PostMapping
-	@Override
-	public void postAndPutObject(@RequestBody Medico object) {
+	public ResponseEntity<Medico> postMedico(@RequestBody Medico object) {
+		if (object.getId() != null) {
+			var medico = getById(object.getId().intValue());
+			
+			if (medico.hasBody()) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+			}
+		}
+		
 		medicoDB.save(object);
+			
+		return ResponseEntity.ok(object);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Medico> putMedico(@RequestBody Medico object) {
+		if (object.getId() == null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+		}
+		
+		var medico = getById(object.getId().intValue());
+		
+		if (!medico.hasBody()) {
+			return medico;
+		}
+		
+		medicoDB.save(object);
+			
+		return ResponseEntity.ok(object);
 	}
 
 	@DeleteMapping("/{id}")
-	@Override
-	public void deleteObject(@PathVariable("id") Integer id) {
-		medicoDB.deleteById((long)id);
+	public ResponseEntity<Medico> deleteMedico(@PathVariable("id") Integer id) {
+		var medico = getById(id);
+		
+		if (!medico.hasBody()) {
+			return medico;
+		}
+		
+		medicoDB.deleteById((long) id);
+		
+		return medico;
 	}
 
 	@GetMapping("/{id}")
-	@Override
-	public Medico getById(@PathVariable("id") Integer id) {
-		return medicoDB.getOne((long)id);
+	public ResponseEntity<Medico> getById(@PathVariable("id") Integer id) {
+		try {
+			var medico = medicoDB.findById((long) id);
+			
+			return ResponseEntity.ok(medico.get());
+		} catch (NoSuchElementException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
 	}
-
- 
 }
--- back-end/src/main/java/com/skynet/skymed/model/Medico.java
+++ back-end/src/main/java/com/skynet/skymed/model/Medico.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
+import com.skynet.skymed.util.Validadores;
+
 @Entity
 public class Medico {
 
@@ -52,7 +54,13 @@ public class Medico {
 		return nome;
 	}
 
-	public void setNome(String nome) {
+	public void setNome(String nome) throws Exception {
+		if (nome == null) {
+			throw new Exception("Nome inválido.");
+		}
+		if (nome.isEmpty() || nome.length() < 5) {
+			throw new Exception("Nome inválido.");
+		}
 		this.nome = nome;
 	}
 	
@@ -60,7 +68,13 @@ public class Medico {
 		return registro;
 	}
 
-	public void setRegistro(String registro) {
+	public void setRegistro(String registro) throws Exception {
+		if (registro == null) {
+			throw new Exception("Registro inválido.");
+		}
+		if (registro.isEmpty() || registro.length() < 5) {
+			throw new Exception("Registro inválido.");
+		}
 		this.registro = registro;
 	}
 
@@ -68,7 +82,13 @@ public class Medico {
 		return rg;
 	}
 
-	public void setRg(String rg) {
+	public void setRg(String rg) throws Exception {
+		if (rg == null) {
+			throw new Exception("RG inválido.");
+		}
+		if (rg.isEmpty() || rg.length() < 6) {
+			throw new Exception("RG inválido.");
+		}
 		this.rg = rg;
 	}
 
@@ -76,7 +96,10 @@ public class Medico {
 		return telefone;
 	}
 
-	public void setTelefone(String telefone) {
+	public void setTelefone(String telefone) throws Exception {
+		if (!Validadores.celularEhValido(telefone)) {
+			throw new Exception("Telefone inválido.");
+		}
 		this.telefone = telefone;
 	}
 
@@ -84,7 +107,11 @@ public class Medico {
 		return email;
 	}
 
-	public void setEmail(String email) {
+	public void setEmail(String email) throws Exception {
+		if (!Validadores.emailEhValido(email)) {
+			throw new Exception("E-mail não é valido.");
+		}
+
 		this.email = email;
 	}
 
@@ -100,7 +127,11 @@ public class Medico {
 		return cpf_cnpj;
 	}
 
-	public void setCpf_cnpj(String cpf_cnpj) {
+	public void setCpf_cnpj(String cpf_cnpj) throws Exception {
+		if (!Validadores.cpfCnpjEhValido(cpf_cnpj)) {
+			throw new Exception("Cpf/Cnpj não é valido.");
+		}
+		
 		this.cpf_cnpj = cpf_cnpj;
 	}
 
@@ -116,7 +147,10 @@ public class Medico {
 		return endereco;
 	}
 
-	public void setEndereco(Endereco endereco) {
+	public void setEndereco(Endereco endereco) throws Exception {
+		if (endereco == null) {
+			throw new Exception("Endereço inválido.");
+		}
 		this.endereco = endereco;
 	}
 
