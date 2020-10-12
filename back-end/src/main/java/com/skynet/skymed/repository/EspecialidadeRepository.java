13ec49fff2c2a21f63b3c97d360144b55bab8d7b
dearaujojr <dearaujojr@gmail.com>
2020-10-12 15:07:10 -0300

    Criação - Criado Classe Repository de Especialidade

---
+++ back-end/src/main/java/com/skynet/skymed/repository/EspecialidadeRepository.java
@@ -0,0 +1,12 @@
+package com.skynet.skymed.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.skynet.skymed.model.Especialidade;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
+
+}
