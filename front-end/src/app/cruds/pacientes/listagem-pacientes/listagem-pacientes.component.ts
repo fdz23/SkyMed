5a36990948d9eb1029dfac45e18de224acdb86cd
Raphael Mota <raphael.mota14@hotmail.com>
2020-10-12 10:52:19 -0300

    remocao-metodo

--- front-end/src/app/cruds/pacientes/listagem-pacientes/listagem-pacientes.component.ts
+++ front-end/src/app/cruds/pacientes/listagem-pacientes/listagem-pacientes.component.ts
@@ -33,24 +33,5 @@ export class ListagemPacientesComponent implements OnInit {
     this.pacienteService.pegaListagemPaciente().subscribe(pacientes => this.pacientes = pacientes);
 
   }
-
-  deletaPaciente(id: number) {
-
-    this.confirmationService.confirm({
-      message: 'Você deseja realmente excluir o cadastro?',
-      header: 'Confirmation',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.pacienteService.deletaPaciente(id).subscribe(paciente => { }, erro => { console.log('Erro ao deletar Paciente') });
-        this.pegarListagemPaciente();
-        this.msgs = [{ severity: 'info', summary: 'Confirmed', detail: 'Cadastro Excluído' }];
-      },
-      reject: () => {
-        this.msgs = [{ severity: 'info', summary: 'Rejected', detail: 'Operação Cancelada' }];
-      }
-    });
-
-  }
-
-
+  
 }
