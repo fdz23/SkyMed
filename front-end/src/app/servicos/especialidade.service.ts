a5261720f3db2c170c953f7be4702f4ea0c68df1
dearaujojr <dearaujojr@gmail.com>
2020-10-12 14:09:27 -0300

    Criação - Criado serviço de interação com API

---
+++ front-end/src/app/servicos/especialidade.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Especialidades } from 'src/assets/especialidades';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EspecialidadeService {
+
+  constructor(private http: HttpClient) { }
+
+  insereEspecialidade(medico: Especialidades): Observable<Especialidades> {
+    const url = `http://127.0.0.1:8080/especialidade/`;
+    return this.http.post<Especialidades>(url, medico);
+  }
+}
