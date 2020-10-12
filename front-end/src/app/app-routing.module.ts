48f0434763bdfa2c6ede0b6c3f8fc52c706c05f1
dearaujojr <dearaujojr@gmail.com>
2020-10-12 14:05:11 -0300

    Criação - Criada rota do cadastro de especialidade

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { CreatePacienteComponent } from './cruds/pacientes/create-paciente/creat
 import { HomeComponent } from './navegacao/home/home.component';
 import { CreateHospitalComponent } from './cruds/hospitais/create-hospital/create-hospital.component';
 import { LoginComponent } from './acessos/login/login.component';
+import { CreateEspecialidadeComponent } from './cruds/especialidades/create-especialidade/create-especialidade.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component : HomeComponent },
   { path: 'medico-criar', component: CreateMedicoComponent },
-  { path: 'paciente-criar', component: CreatePacienteComponent},
-  { path: 'hospital-criar', component: CreateHospitalComponent},
+  { path: 'paciente-criar', component: CreatePacienteComponent },
+  { path: 'hospital-criar', component: CreateHospitalComponent },
+  { path: 'especialidade-criar', component: CreateEspecialidadeComponent },
   { path: 'login', component: LoginComponent },
 
 
