ed9795fc8693d33cb57858251b9e258bfce1c7fc
fdz23 <ferdavid_9@hotmail.com>
2020-10-12 21:24:06 -0300

    Correção - Ajusta testes conforme modelo

--- back-end/src/test/java/com/skynet/skymed/model/MedicoTest.java
+++ back-end/src/test/java/com/skynet/skymed/model/MedicoTest.java
@@ -21,6 +21,7 @@ class MedicoTest {
 	private Medico inicializarMedico() throws Exception {
 		var medico = new Medico();
 		var endereco = new Endereco();
+		var pessoa = new Pessoa();
 		
 		endereco.setCep("74520-110");
 		endereco.setComplemento("Qd. 5-i Lt. 5");
@@ -30,108 +31,22 @@ class MedicoTest {
 		endereco.setNumero("23");
 		endereco.setUf("GO");
 		
-		medico.setCpf_cnpj("064.240.911-06");
-		medico.setEhPaciente(false);
-		medico.setEmail("ferdavid_9@hotmail.com");
-		medico.setEndereco(endereco);
-		medico.setNome("Fernando David");
+		pessoa.setCpf("064.240.911-06");
+		pessoa.setEhPaciente(false);
+		pessoa.setEmail("ferdavid_9@hotmail.com");
+		pessoa.setNome("Fernando David");
+		pessoa.setRg("6349-622");
+		pessoa.setTelefone("(62) 99854-0164");
+
 		medico.setRegistro("1234");
-		medico.setRg("6349-622");
-		medico.setTelefone("(62) 99854-0164");
+		medico.setPessoa(pessoa);
+		pessoa.setEndereco(endereco);
 		
 		return medico;
 	}
 
 	@Test
 	void insereCamposInvalidos() {
-		try {
-			medico.setCpf_cnpj(null);
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-		
-		try {
-			medico.setCpf_cnpj("           ");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setCpf_cnpj("aaa.aaa.aaa-aa");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setCpf_cnpj("00000000000");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setCpf_cnpj("              ");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setCpf_cnpj("00000000000000");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-		
-		try {
-			medico.setCpf_cnpj("000.000.000/000-00");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setCpf_cnpj("aaa.aaa.aaa/aaa-aa");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setNome("");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setNome(null);
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setNome("aaaa");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
 		try {
 			medico.setRegistro("");
 			
@@ -155,129 +70,9 @@ class MedicoTest {
 		} catch (Exception e) {
 			assertNotNull(e);
 		}
-
+		
 		try {
-			medico.setRg("");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setRg(null);
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setRg("aaaaa");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setTelefone("");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setTelefone(null);
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setTelefone("62 998540164");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setTelefone("62 99854-0164");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setTelefone("(62 998540164");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setTelefone("62) 998540164");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setTelefone("(62)99854-0164");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setEmail("");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setEmail(null);
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setEmail("aaaaaaa@aaaaa");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setEmail("@sfaer.com");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setEmail("      ");
-			
-			Assert.fail();
-		} catch (Exception e) {
-			assertNotNull(e);
-		}
-
-		try {
-			medico.setEndereco(null);
+			medico.setPessoa(null);
 			
 			Assert.fail();
 		} catch (Exception e) {
