63e3a0ecfd1e4506fee07a641084f515361cc99d
fdz23 <ferdavid_9@hotmail.com>
2020-10-12 21:28:24 -0300

    Criação - Adiciona métodos crud

--- front-end/src/app/servicos/medico.service.ts
+++ front-end/src/app/servicos/medico.service.ts
@@ -14,4 +14,25 @@ export class MedicoService {
     const url = `http://127.0.0.1:8080/medico/`;
     return this.http.post<Medicos>(url, medico);
   }
+
+  atualizaMedico(medico: Medicos): Observable<Medicos> {
+    const url = `http://127.0.0.1:8080/medico/`;
+    return this.http.put<Medicos>(url, medico);
+  }
+
+  obtenhaMedicos(): Observable<any> {
+    const url = `http://127.0.0.1:8080/medico/`;
+    return this.http.get<Medicos[]>(url);
+
+  }
+
+  obtenhaMedicoPorId(id: any): Observable<any> {
+    const url = `http://127.0.0.1:8080/medico/`;
+    return this.http.get<Medicos[]>(url.concat(id));
+  }
+
+  deletaMedico(id: any): Observable<any> {
+    const url = `http://127.0.0.1:8080/medico/`;
+    return this.http.delete(url.concat(id));
+  }
 }
