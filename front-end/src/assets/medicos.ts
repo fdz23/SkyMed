873b9608ae5509e5200c31204d17d9f19ad6f1ea
fdz23 <ferdavid_9@hotmail.com>
2020-11-01 18:50:20 -0300

    Criadas e ajustadas interfaces frontend

--- front-end/src/assets/medicos.ts
+++ front-end/src/assets/medicos.ts
@@ -1,9 +1,11 @@
 import { Especialidades } from './especialidades';
 import { Pessoas } from './Pessoas';
+import { Horarios } from './Horarios';
 
 export interface Medicos {
   id: number;
   registro: string;
-  especialidade: Especialidades[];
+  especialidade: Especialidades;
   pessoa: Pessoas;
+  horarios: Horarios[];
 }
