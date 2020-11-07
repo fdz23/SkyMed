e800e8676d04c69bc7b4c93f9d8ccf5b1522d013
Raphael Mota <raphael.mota14@hotmail.com>
2020-11-07 19:54:59 -0300

    Criação - Criado entidade usuário

--- back-end/src/main/java/com/skynet/skymed/model/Usuario.java
+++ back-end/src/main/java/com/skynet/skymed/model/Usuario.java
@@ -23,44 +23,73 @@ public class Usuario extends AbstractEntity {
 	@NotNull
 	@Column(name = "usu_senha")
 	private String senha;
-	@NotNull
+
 	@Column(name = "usu_token_autenticacao")
 	private String token_autenticacao;
-	
+
+	@Column(name = "usu_token_redefinicao")
+	private String token_redefinicao;
+
 	@Column(name = "usu_admin")
 	private boolean admin;
-	
+
+	public String getToken_redefinicao() {
+		return token_redefinicao;
+	}
+
+	public void setToken_redefinicao(String token_redefinicao) {
+		this.token_redefinicao = token_redefinicao;
+	}
+
+	@Column(name = "usu_medico")
+	private boolean medico;
+
 	public boolean isAdmin() {
 		return admin;
 	}
+
+	public boolean isMedico() {
+		return medico;
+	}
+
+	public void setMedico(boolean medico) {
+		this.medico = medico;
+	}
+
 	public void setAdmin(boolean admin) {
 		this.admin = admin;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public String getToken_autenticacao() {
 		return token_autenticacao;
 	}
+
 	public void setToken_autenticacao(String token_autenticacao) {
 		this.token_autenticacao = token_autenticacao;
 	}
-	
-  
-	
+
 }
