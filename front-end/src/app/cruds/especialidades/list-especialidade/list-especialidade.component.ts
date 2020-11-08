c28bfd26d0fec1a19481d4c5f4d3920d72cde90a
dearaujojr <dearaujojr@gmail.com>
2020-11-07 21:45:13 -0300

    Listar Especialidade Implementado - melhorar HTML

--- front-end/src/app/cruds/especialidades/list-especialidade/list-especialidade.component.ts
+++ front-end/src/app/cruds/especialidades/list-especialidade/list-especialidade.component.ts
@@ -19,13 +19,54 @@ export class ListEspecialidadeComponent implements OnInit {
   msgs: Message[] = [];
   @ViewChild('dt') table: Table;
 
-  constructor(private especialidadeService: EspecialidadeService, private router: Router, private confirmationService: ConfirmationService) { }
+  constructor(
+    private especialidadeService: EspecialidadeService,
+    private router: Router,
+    private confirmationService: ConfirmationService) { }
 
   ngOnInit(): void {
     this.obtenhaEspecialidades();
   }
+  // tslint:disable-next-line: typedef
   obtenhaEspecialidades() {
-    this.especialidadeService.obtenhaEspecialidades().subscribe(especialidades => this.especialidades = especialidades);
+    this.especialidadeService.obtenhaEspecialidades()
+    .subscribe(
+      especialidades => {
+        this.especialidades = especialidades;
+      });
+  }
+
+  deleteEspecialidade(id) {
+
+    this.confirmationService.confirm({
+      message: 'Deseja realmente excluir a especialidade?',
+      header: 'Exclusão de especialidade',
+      icon: 'pi pi-info-circle',
+
+      accept: () => {
+        this.especialidadeService.deletaEspecialidade(id).subscribe(especialidade => {
+          this.msgs = [];
+          this.msgs = [{ severity: 'info', summary: 'Concluído', detail: 'Especialidade Excluida' }];
+          window.location.reload;
+
+        },
+
+          error => {
+            this.msgs = [];
+            this.msgs.push({ severity: 'error', detail: `Erro ao deletar Especialidade : ${error.error}` });
+          }
+
+        );
+
+        this.confirmationService.close();
+      },
+      reject: () => {
+        this.msgs = [];
+        this.msgs = [{ severity: 'info', summary: 'Cancelado', detail: 'Operação Cancelada' }];
+        this.confirmationService.close();
+      }
+
+    });
   }
 
 }
