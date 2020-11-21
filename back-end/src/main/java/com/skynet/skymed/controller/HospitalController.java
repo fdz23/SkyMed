b8f38fe46fe0867b5bbb9d1d3bddff98ccf81b66
fdz23 <ferdavid_9@hotmail.com>
2020-11-21 19:28:31 -0300

    Criação - Implementa verificações nas controllers

--- back-end/src/main/java/com/skynet/skymed/controller/HospitalController.java
+++ back-end/src/main/java/com/skynet/skymed/controller/HospitalController.java
@@ -1,12 +1,13 @@
 package com.skynet.skymed.controller;
 
-import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,34 +18,48 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.skynet.skymed.model.Hospital;
 import com.skynet.skymed.repository.HospitalRepository;
+import com.skynet.skymed.service.ValidacaoPessoaService;
 
 @RestController
 
 @RequestMapping("/hospital")
 public class HospitalController {
+	private final String HOSPITAL_INEXISTENTE = "Hospital inexistente.";
+	private final ValidacaoPessoaService validacaoPessoa = new ValidacaoPessoaService();
 
 	@Autowired
 	private HospitalRepository hospitalDB;
+	
+	@ExceptionHandler({ HttpMessageNotReadableException.class })
+    public ResponseEntity<Object> handleException(HttpMessageNotReadableException ex) {
+		return ResponseEntity.badRequest().body(ex.getMostSpecificCause().getMessage());
+    }
 
 	@GetMapping
-	public ResponseEntity<ArrayList<Hospital>> getObject() {
+	public ResponseEntity<Object> getObject() {
 		var hospitais = hospitalDB.findAll();
 		
 		if (hospitais.size() == 0) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi encontrado nenhum hospital.");
 		}
 		
-		return ResponseEntity.ok((ArrayList<Hospital>) hospitais);
+		return ResponseEntity.ok(hospitais);
 	}
 
 	@PostMapping
-	public ResponseEntity<Hospital> postHospital(@RequestBody Hospital object) {
+	public ResponseEntity<Object> postHospital(@RequestBody Hospital object) {
 		if (object.getId() != null) {
 			var hospital = getById(object.getId().intValue());
 			
-			if (hospital.hasBody()) {
-				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+			if (!hospital.getBody().equals(HOSPITAL_INEXISTENTE)) {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Hospital existente.");
 			}
+		}
+		
+		var validacao = validacaoPessoa.valideInsercao(object.getPessoa());
+		
+		if (validacao != null) {
+			return validacao;
 		}
 		
 		hospitalDB.save(object);
@@ -53,15 +68,21 @@ public class HospitalController {
 	}
 	
 	@PutMapping
-	public ResponseEntity<Hospital> putHospital(@RequestBody Hospital object) {
+	public ResponseEntity<Object> putHospital(@RequestBody Hospital object) {
 		if (object.getId() == null) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Id inválido.");
 		}
 		
 		var hospital = getById(object.getId().intValue());
 		
-		if (!hospital.hasBody()) {
+		if (hospital.getBody().equals(HOSPITAL_INEXISTENTE)) {
 			return hospital;
+		}
+		
+		var validacao = validacaoPessoa.valideAtualizacao(object.getPessoa());
+		
+		if (validacao != null) {
+			return validacao;
 		}
 		
 		hospitalDB.save(object);
@@ -70,26 +91,26 @@ public class HospitalController {
 	}
 
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Hospital> deleteHospital(@PathVariable("id") Integer id) {
+	public ResponseEntity<Object> deleteHospital(@PathVariable("id") Integer id) {
 		var hospital = getById(id);
 		
-		if (!hospital.hasBody()) {
+		if (hospital.getBody().equals(HOSPITAL_INEXISTENTE)) {
 			return hospital;
 		}
 		
 		hospitalDB.deleteById((long) id);
 		
-		return hospital;
+		return ResponseEntity.ok(null);
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Hospital> getById(@PathVariable("id") Integer id) {
+	public ResponseEntity<Object> getById(@PathVariable("id") Integer id) {
 		try {
 			var hospital = hospitalDB.findById((long) id);
 			
 			return ResponseEntity.ok(hospital.get());
 		} catch (NoSuchElementException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(HOSPITAL_INEXISTENTE);
 		}
 	}
 }
