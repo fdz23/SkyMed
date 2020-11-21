82f1dfd13b6e4132fa794b6014ea99ae5dbfcea5
fdz23 <ferdavid_9@hotmail.com>
2020-11-21 19:28:20 -0300

    Criação - Cria serviço para validar campos de pessoa já existentes

---
+++ back-end/src/main/java/com/skynet/skymed/service/ValidacaoPessoaService.java
@@ -0,0 +1,38 @@
+package com.skynet.skymed.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.skynet.skymed.model.Pessoa;
+import com.skynet.skymed.repository.PessoaRepository;
+
+@Service
+public class ValidacaoPessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaDB;
+	
+	public ResponseEntity<Object> valideInsercao(Pessoa pessoa) {
+		if (pessoaDB.findByCpf(pessoa.getCpf()) != null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cpf existente.");
+		} else if (pessoaDB.findByRg(pessoa.getRg()) != null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("RG existente.");
+		} else if (pessoaDB.findByUsuarioEmail(pessoa.getUsuario().getEmail()) != null) {
+		 	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("E-mail existente.");
+		} else {
+			return null;
+		}
+	}
+	
+	public ResponseEntity<Object> valideAtualizacao(Pessoa pessoa) {
+		var pessoaCpf = pessoaDB.findByCpf(pessoa.getCpf());
+		
+		if (pessoaCpf != null && !pessoaCpf.getUsuario().getEmail().equals(pessoa.getUsuario().getEmail())) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("E-mail existente");
+		} else {
+			return null;
+		}
+	}
+}
