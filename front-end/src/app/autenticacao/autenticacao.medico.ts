5c090a0ff38a3dfd5ca461ca7581da17ae4c0433
fdz23 <ferdavid_9@hotmail.com>
2020-11-22 20:18:41 -0300

    Criação - Adiciona classes que autenticam baseado na role

---
+++ front-end/src/app/autenticacao/autenticacao.medico.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+import { AutenticacaoService } from './autenticacao.service';
+
+@Injectable({ providedIn: 'root' })
+export class AutenticacaoMedico implements CanActivate {
+    constructor(
+        private router: Router,
+        private authenticationService: AutenticacaoService
+    ) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        const currentUser = this.authenticationService.currentUserValue;
+        if (currentUser && (currentUser.ehMedico || currentUser.ehAdmin)) {
+            return true;
+        }
+
+        this.router.navigate(['/home'], { queryParams: { returnUrl: state.url }});
+        return false;
+    }
+}
