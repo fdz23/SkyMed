08df3b319b306d779bcdd17d2a42703b38de8021
Raphael Mota <raphael.mota14@hotmail.com>
2020-11-29 19:10:05 -0300

    Criação - Criado tela de erro de autorização

---
+++ front-end/src/app/navegacao/autorizacao/autorizacao.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-autorizacao',
+  templateUrl: './autorizacao.component.html'
+})
+export class AutorizacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
