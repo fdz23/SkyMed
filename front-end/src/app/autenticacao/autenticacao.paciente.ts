b64580370d222b5b2cbdc3cd216677d63b593590
Raphael Mota <raphael.mota14@hotmail.com>
2020-12-02 23:21:25 -0300

    Melhoria - Ajustado redirecionamento para tela de erro de autorização

--- front-end/src/app/autenticacao/autenticacao.paciente.ts
+++ front-end/src/app/autenticacao/autenticacao.paciente.ts
@@ -16,7 +16,7 @@ export class AutenticacaoPaciente implements CanActivate {
             return true;
         }
 
-        this.router.navigate(['/home'], { queryParams: { returnUrl: state.url }});
+        this.router.navigate(['/erro-autorizacao']);
         return false;
     }
 }
