4b97a85d5ccf73ee895c00027baa53bedfbdcf71
Raphael Mota <raphael.mota14@hotmail.com>
2020-12-03 14:04:08 -0300

    Criação - Adicionado verificação se o usuário tem conta autenticada

--- front-end/src/app/acessos/login/login.component.ts
+++ front-end/src/app/acessos/login/login.component.ts
@@ -13,39 +13,63 @@ import { Usuarios } from '../../../assets/usuarios';
 export class LoginComponent implements OnInit {
 
   constructor(private autenticacaoService: AutenticacaoService,
-              private usuarioService: UsuarioService,
-              private router: Router) {}
+    private usuarioService: UsuarioService,
+    private router: Router) { }
 
   senha: string;
   email: string;
   msgs: Message[] = [];
   feedback = false;
+  ehAutenticado: boolean;
 
   ngOnInit(): void { }
 
   login(): void {
-    const usuario = { email: this.email, senha: this.senha } as Usuarios;
-    this.autenticacaoService.login(usuario).subscribe(
-      resposta => {
-        this.usuarioService.obtemUsuario(usuario).subscribe(
-          user => {
-            user.token_autenticacao = resposta.token;
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            this.autenticacaoService.currentUserSubject.next(user);
-            window.location.reload();
-            this.msgs = [];
-          },
-          erro => {
-            this.msgs = [];
-            this.msgs.push({ severity: 'error', detail: `${erro.error}` });
-          }
-        );
-      },
-      erro => {
-        this.msgs = [];
-        this.msgs.push({ severity: 'error', detail: `${erro.error}` });
+
+    if (this.usuarioEhAutenticado()) {
+      const usuario = { email: this.email, senha: this.senha } as Usuarios;
+      this.autenticacaoService.login(usuario).subscribe(
+        resposta => {
+          this.usuarioService.obtemUsuario(usuario).subscribe(
+            user => {
+              user.tokenAutenticacao = resposta.token;
+              localStorage.setItem('currentUser', JSON.stringify(user));
+              this.autenticacaoService.currentUserSubject.next(user);
+              window.location.reload();
+              this.msgs = [];
+            },
+            erro => {
+              this.msgs = [];
+              this.msgs.push({ severity: 'error', detail: `${erro.error}` });
+            }
+          );
+        },
+        erro => {
+          this.msgs = [];
+          this.msgs.push({ severity: 'error', detail: `${erro.error}` });
+        }
+      );
+    }
+  }
+
+  usuarioEhAutenticado(): boolean {
+
+    this.usuarioService.obtemUsuarioPorEmail(this.email).subscribe(
+      (usuario: Usuarios) => {
+
+        this.ehAutenticado = usuario.ehAutenticado;
+
+        if (!usuario.ehAutenticado) {
+
+          this.router.navigateByUrl("autenticacao-conta/".concat(this.email));
+
+          return false;
+
+        }
       }
+
     );
+    return true;
   }
 
 }
