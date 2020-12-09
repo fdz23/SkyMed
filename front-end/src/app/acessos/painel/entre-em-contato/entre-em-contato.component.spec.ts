3dc086c3023d41fc49e1b6121e8287976b0a053f
fdz23 <ferdavid_9@hotmail.com>
2020-12-08 21:14:46 -0300

    Criação - Cria tela para hospitais logados

---
+++ front-end/src/app/acessos/painel/entre-em-contato/entre-em-contato.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EntreEmContatoComponent } from './entre-em-contato.component';
+
+describe('EntreEmContatoComponent', () => {
+  let component: EntreEmContatoComponent;
+  let fixture: ComponentFixture<EntreEmContatoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EntreEmContatoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EntreEmContatoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ front-end/src/app/acessos/painel/entre-em-contato/entre-em-contato.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entre-em-contato',
+  templateUrl: './entre-em-contato.component.html'
+})
+export class EntreEmContatoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
